c227bf7da656fe37f1aee517861b22b4113018a9
Bob Rusch <rvrusch@gmail>
2021-01-21 14:01:57 -0500

    Updates

---
+++ precache-manifest.f62c7923e27bed90cba7b4cf2de0b78a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba136301fd4d0079ecef5818e6b072e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "064cc09555fd3103db8c",
+    "url": "/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "7174a9a1051f6373b2f0",
+    "url": "/static/css/main.9a8415d2.chunk.css"
+  },
+  {
+    "revision": "064cc09555fd3103db8c",
+    "url": "/static/js/2.8d2d3d32.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8d2d3d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7174a9a1051f6373b2f0",
+    "url": "/static/js/main.ae072b84.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
